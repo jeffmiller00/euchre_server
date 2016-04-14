1999ccf4139556813e9ac06ae19bd911d5341473
Jeff Miller <jeffmiller00@gmail.com>
2016-04-14 15:45:12 -0500

    Skip the controller tests for now and refactor the game tests.

--- test/controllers/game_controller_test.rb
+++ test/controllers/game_controller_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 
 class GameControllerTest < ActionController::TestCase
   test 'you can create a new game' do
+    skip 'Rewriting the controller.'
     response = post :new
     assert_equal "{\"status\":\"Need 4 more players.\"}", response.body
   end
 
   test 'that only the dealer can deal' do
+    skip 'Rewriting the controller.'
     game = Game.create
     player_codes = []
     ('A'..'D').to_a.each do |player|
@@ -21,6 +23,7 @@ class GameControllerTest < ActionController::TestCase
 
   # Moved this test to the bottom because the syntax highlighting is messed up.
   test 'you can join a game' do
+    skip 'Rewriting the controller.'
     game = Game.create
     response = post :join_game, id: game.id, name: 'Jeff'
     #assert_match /\"\{\"id\":d*,\"code\":[\w]\}\"/, response.body
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -14,31 +14,16 @@ class GameTest < ActiveSupport::TestCase
     assert_equal 4, euchre.players.size
   end
 
-  test 'initial game status' do
+  test 'allow people to join and verify state' do
     euchre = Game.new
-    assert_equal 'Need 4 more players.', euchre.status
-  end
-
-  test 'allow people to join and verify status' do
-    euchre = Game.new
-    assert_equal 'Need 4 more players.', euchre.status
-    euchre.join_game 'a'
-    assert_equal 'Need 3 more players.', euchre.status
-    euchre.join_game 'b'
-    assert_equal 'Need 2 more players.', euchre.status
-    euchre.join_game 'c'
-    assert_equal 'Need 1 more players.', euchre.status
-    euchre.join_game 'd'
-    assert_match /It\'s [\w]\'s deal./, euchre.status
+    assert_equal 'need_players', euchre.state
+    4.times { euchre.join_game Faker::Name.name }
+    assert_equal 'declaring_trump', euchre.state
   end
 
   test 'after the deal, each player has five cards' do
     euchre = Game.new
-    euchre.join_game 'a'
-    euchre.join_game 'b'
-    euchre.join_game 'c'
-    euchre.join_game 'd'
-    euchre.deal!
+    4.times { euchre.join_game Faker::Name.name }
     euchre.players.each do |player|
       assert_equal 5, player.hand.cards.size
     end
